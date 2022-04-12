2a41532fea3c7bd00ec291b8e6b03202970b202d
Sujeewa Sandeepa <sujeewasandeepa@protonmail.ch>
2022-04-12 13:03:26 +0530

    create dao and connect it on index.js

--- backend/api/dao/restaurantDAO.js
+++ backend/api/dao/restaurantDAO.js
@@ -12,3 +12,52 @@ export default class RestaurantsDAO {
         }
     }
 }
+
+const getRestaurants = async ({
+    filters = null,
+    page = 0,
+    restaurantsPerPage = 20,
+}) => {
+    let query;
+
+    if (filter) {
+        if ("name" in filters) {
+            query = { $text: { $search: filters["name"] }}
+        } else if ("cuisine" in filters) {
+            query = { "cuisine": { $eq: filters["cuisine"] }}
+        } else if ("zipcode" in filters) {
+            query = { "address.zipcode": { $eq: filters["zipcode"] }}
+        }
+    }
+
+    let cursor;
+
+    try {
+        cursor = await restaurants
+            .find(query);
+    } catch (e) {
+        console.error(`Unable to issue find command, ${ e }`);
+        return { restaurantsList: [], totalNumRestaurants: 0 }
+    }
+
+    const displayCursor = cursor.limit(restaurantsPerPage).skip(restaurantsPerPage * page);
+
+    try {
+        const restaurantsList = await displayCursor.toArray();
+        const totalNumRestaurants = await restaurants.countDocuments(query);
+
+        return { restaurantsList, totalNumRestaurants };
+    } catch (e) {
+        console.error(
+            `Unable to convert cursor to array or problem counting documents, ${ e }`
+        );
+        return { restaurantsList: [], totalNumRestaurants: 0 };
+    }
+}
+// static async getRestaurants ({
+//     filters = null,
+//     page = 0,
+//     restaurantsPerPage = 20,
+// } = {}) {
+ 
+// }
